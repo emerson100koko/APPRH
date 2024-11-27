163006c91335fbafe240e02fedc68ec58e3d343f
Emerson Gomes Nunes <emerson100loko@hotmail.com>
2024-11-26 22:03:16 -0300

    some controllers

--- src/main/java/com/AppRH/AppRH/controllers/VagaController.java
+++ src/main/java/com/AppRH/AppRH/controllers/VagaController.java
@@ -42,14 +42,17 @@ public class VagaController {
 	
 	@RequestMapping("/vagas")
 	public ModelAndView listaVagas() {
+		
 		ModelAndView mv = new ModelAndView("vaga/listaVaga");
 		Iterable<Vaga> vagas = vr.findAll();
 		mv.addObject("vagas", vagas);
 		return mv;
+		
 	}
 	
 	@RequestMapping(value = "/{codigo}", method = RequestMethod.GET)
 	public ModelAndView detalhesDaVaga(@PathVariable("codigo") long codigo) {
+		
 		Vaga vaga = vr.findByCodigo(codigo);
 		ModelAndView mv = new ModelAndView("vaga/detalhesVaga");
 		mv.addObject("vaga", vaga);
@@ -61,5 +64,34 @@ public class VagaController {
 		
 	}
 	
+	@RequestMapping("/deletarVaga")
+	public String deletarVaga(long codigo) {
+		
+		Vaga vaga = vr.findByCodigo(codigo);
+		vr.delete(vaga);
+		return "redirect:/vagas";
+		
+	}
+	
+	public String detalhesVagaPost(@PathVariable("codigo") long codigo, @Valid Canditado candidato, 
+			BindingResult result, RedirectAttributes attributes) {
+		
+		if(result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique os campos");
+			return "redirect:/{codigo}";
+		}
+		
+		if(cr.findByRg(candidato.getRg()) != null) {
+			attributes.addFlashAttribute("mensagem erro", "RG duplicado");
+			return "redirect:/{codigo}";
+		}
+		
+		Vaga vaga = vr.findByCodigo(codigo);
+		candidato.setVaga(vaga);
+		cr.save(candidato);
+		attributes.addFlashAttribute("mensagem", "Candidato Adicionado com sucesso!");
+		return "redirect:/{codigo}";
+		
+	}
 	
 }
